bfed24e6b1752dfc7b1dddd0f20ec734b54c6fab
reece-elder <reece.elder@qa.com>
2022-03-17 14:49:26 +0000

    get by id test

--- DFESW13-HolidayBookings/src/test/java/com/qa/qaHoliday/testing/controller/ControllerTest.java
+++ DFESW13-HolidayBookings/src/test/java/com/qa/qaHoliday/testing/controller/ControllerTest.java
@@ -1,10 +1,13 @@
 package com.qa.qaHoliday.testing.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.qaHoliday.model.HolidayBooking;
  
@@ -22,6 +26,7 @@ import com.qa.qaHoliday.model.HolidayBooking;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 // Set up our Mock MVC (replacing postman) to work with the project
 @AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // Allows us to use @BeforeAll in a NON STATIC way
 public class ControllerTest {
 	
 	
@@ -33,6 +38,15 @@ public class ControllerTest {
 	
 	// Test Object
 	HolidayBooking testBooking = new HolidayBooking("test country", "test weather", 1, true);
+	
+	// This will run before all of the other tests once
+	@BeforeAll
+	public void setup() throws Exception {
+		// Before any tests run, I want there to be atleast one test object in the database
+		String bookingJson = mapper.writeValueAsString(testBooking);
+		RequestBuilder req = post("/create").contentType(MediaType.APPLICATION_JSON).content(bookingJson);
+		mvc.perform(req);
+	}
 	
 	// Spring comes with JUnit 5 and Mockito
 	
@@ -62,6 +76,27 @@ public class ControllerTest {
 		// tell our mvc (postman mock) to run the request (click send)
 		// run the request and checkStatus SHOULD be true (status code is correct) 
 		// AND checkBody SHOULD Be true (response body is correct) 
+		mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+		
+	}
+	@Test
+	public void testGetId() throws Exception {
+		
+		// Arrange 
+		HolidayBooking testBookingId = testBooking; // Making a new booking obj equal to our testBooking Object
+		testBookingId.setId(6l); // Setting the id of our object to be 6, identical to how it will be
+								 // when we get it from the database
+		
+		String testBookingIdJson = mapper.writeValueAsString(testBookingId); // converting our obj (with id) to JSON
+		
+		// Act 
+		// Passing in the path variable 6, but is added as part of the string
+		RequestBuilder req = get("/getId/6");
+		
+		ResultMatcher checkStatus = status().isAccepted(); // is the response status code accepted?
+		ResultMatcher checkBody = content().json(testBookingIdJson); // Is the response content a Json String of our object?
+		
+		// Assert
 		mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
 		
 	}
